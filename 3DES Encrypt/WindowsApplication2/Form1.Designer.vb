0cb93c1ba60b1d7e0dbf3da6d232865b77619d2e
Supreme Developer <unconfigured@null.spigotmc.org>
2018-10-23 19:17:48 -0300

    3DES Encrypt v2.0

---
+++ 3DES Encrypt/WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,250 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Tchave = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Ttexto = New System.Windows.Forms.TextBox()
+        Me.ContextMenuStrip2 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Tencryptado = New System.Windows.Forms.TextBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.CopiarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ContextMenuStrip2.SuspendLayout()
+        Me.ContextMenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!)
+        Me.Button1.Location = New System.Drawing.Point(8, 390)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(165, 26)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Encrypt"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Tchave
+        '
+        Me.Tchave.Location = New System.Drawing.Point(10, 171)
+        Me.Tchave.Name = "Tchave"
+        Me.Tchave.Size = New System.Drawing.Size(139, 20)
+        Me.Tchave.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!)
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(9, 153)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Chave :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!)
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(9, 194)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Texto :"
+        '
+        'Ttexto
+        '
+        Me.Ttexto.ContextMenuStrip = Me.ContextMenuStrip2
+        Me.Ttexto.Location = New System.Drawing.Point(10, 212)
+        Me.Ttexto.MaxLength = 2147483647
+        Me.Ttexto.Multiline = True
+        Me.Ttexto.Name = "Ttexto"
+        Me.Ttexto.Size = New System.Drawing.Size(331, 75)
+        Me.Ttexto.TabIndex = 3
+        '
+        'ContextMenuStrip2
+        '
+        Me.ContextMenuStrip2.BackColor = System.Drawing.Color.White
+        Me.ContextMenuStrip2.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1})
+        Me.ContextMenuStrip2.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip2.ShowImageMargin = False
+        Me.ContextMenuStrip2.Size = New System.Drawing.Size(85, 26)
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(84, 22)
+        Me.ToolStripMenuItem1.Text = "Copiar"
+        '
+        'Tencryptado
+        '
+        Me.Tencryptado.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.Tencryptado.Location = New System.Drawing.Point(10, 309)
+        Me.Tencryptado.MaxLength = 2147483647
+        Me.Tencryptado.Multiline = True
+        Me.Tencryptado.Name = "Tencryptado"
+        Me.Tencryptado.Size = New System.Drawing.Size(331, 75)
+        Me.Tencryptado.TabIndex = 5
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.BackColor = System.Drawing.Color.White
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CopiarToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.ShowImageMargin = False
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(85, 26)
+        '
+        'CopiarToolStripMenuItem
+        '
+        Me.CopiarToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.CopiarToolStripMenuItem.Name = "CopiarToolStripMenuItem"
+        Me.CopiarToolStripMenuItem.Size = New System.Drawing.Size(84, 22)
+        Me.CopiarToolStripMenuItem.Text = "Copiar"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!)
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(12, 291)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(74, 15)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Encryptado :"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(178, 390)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(165, 26)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Decrypt"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(332, 138)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 11
+        Me.PictureBox1.TabStop = False
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Black
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Lime
+        Me.Label4.Location = New System.Drawing.Point(12, 122)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(334, 19)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "TRIPLEDES v2.0"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(8, 422)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(335, 26)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "Limpar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.Black
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Lime
+        Me.Label5.Location = New System.Drawing.Point(361, 12)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(155, 45)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "By: ConradoPSA (SUPREMEHACKER)"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(354, 457)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Tencryptado)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Ttexto)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Tchave)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "3DES Encrypt/Decrypt || By: SUPREMEHACKER"
+        Me.ContextMenuStrip2.ResumeLayout(False)
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Tchave As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Ttexto As TextBox
+    Friend WithEvents Tencryptado As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents CopiarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ContextMenuStrip2 As ContextMenuStrip
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label5 As Label
+End Class
---
+++ 3DES Encrypt/WindowsApplication2/Form1.vb
@@ -0,0 +1,126 @@
+﻿Imports System.Security.Cryptography
+
+Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles Tchave.TextChanged
+
+    End Sub
+
+#Region "3D"
+
+    Private Shared Function d(ByVal k As String, ByVal t As String) As String
+        Try
+
+            Dim td As New TripleDESCryptoServiceProvider
+            td.Key = th(k, td.KeySize \ 8)
+            td.IV = th("", td.BlockSize \ 8)
+
+            Dim eb() As Byte = Convert.FromBase64String(t)
+
+            Dim m As New System.IO.MemoryStream
+            Dim ds As New CryptoStream(m,
+            td.CreateDecryptor(),
+            System.Security.Cryptography.CryptoStreamMode.Write)
+            ds.Write(eb, 0, eb.Length)
+            ds.FlushFinalBlock()
+
+            Return System.Text.Encoding.Unicode.GetString(m.ToArray)
+        Catch ex As CryptographicException
+            MessageBox.Show("Error ao descryptar!", "Erro!")
+        End Try
+    End Function
+
+    Private Shared Function e(ByVal k As String, ByVal t As String) As String
+        Dim td As New TripleDESCryptoServiceProvider
+        td.Key = th(k, td.KeySize \ 8)
+        td.IV = th("", td.BlockSize \ 8)
+
+        Dim plaintextBytes() As Byte =
+        System.Text.Encoding.Unicode.GetBytes(t)
+
+        Dim ms As New System.IO.MemoryStream
+
+        Dim encStream As New CryptoStream(ms,
+        td.CreateEncryptor(),
+        System.Security.Cryptography.CryptoStreamMode.Write)
+
+        encStream.Write(plaintextBytes, 0, plaintextBytes.Length)
+        encStream.FlushFinalBlock()
+
+        Return Convert.ToBase64String(ms.ToArray)
+    End Function
+
+    Private Shared Function th(
+    ByVal k As String,
+    ByVal i As Integer) As Byte()
+        Dim s As New SHA1CryptoServiceProvider
+
+        Dim kb() As Byte =
+        System.Text.Encoding.Unicode.GetBytes(k)
+        Dim h() As Byte = s.ComputeHash(kb)
+
+        ReDim Preserve h(i - 1)
+        Return h
+    End Function
+
+
+#End Region
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        Clipboard.SetText(Tencryptado.Text)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e2 As EventArgs) Handles Button1.Click
+        MsgBox(Ttexto.Text)
+        System.IO.File.WriteAllText("C:\Users\Derp\Desktop\M.txt", e(Tchave.Text, Ttexto.Text))
+        Tencryptado.Text = e(Tchave.Text, Ttexto.Text)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Ttexto.Text = d(Tchave.Text, Tencryptado.Text)
+    End Sub
+
+
+    Private Sub CopiarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopiarToolStripMenuItem.Click
+        Try
+
+            Clipboard.SetText(Tencryptado.Text)
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+
+    Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem1.Click
+        Try
+
+            Clipboard.SetText(Ttexto.Text)
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        For Each tbox As Control In Me.Controls
+            If TypeOf tbox Is TextBox Then
+                tbox.Text = Nothing
+            End If
+
+        Next
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.Shift And e.KeyCode = Keys.S Then
+            Me.FormBorderStyle = FormBorderStyle.Sizable
+        End If
+    End Sub
+End Class
